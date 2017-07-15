d59cc7c15ebbdb4441a2c1104900c7656c86dc3b
bobby-lin <mr.linjiahao@gmail.com>
2017-07-15 20:53:38 +0800

    Add ex 2.36

---
+++ ex 2.36.rkt
@@ -0,0 +1,15 @@
+; Exercise 2.36
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      `()
+      (cons (accumulate op init (car seqs))
+            (accumulate-n op init (cdr seqs)))))
+
+(define s (list (list 1 2 3) (list 4 5 6) (list 7 8 9)))
+(accumulate-n + 0 s)
