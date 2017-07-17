82c9a0f9c7cfc6f944531fce77d4eeea897e93ad
bobby-lin <mr.linjiahao@gmail.com>
2017-07-17 21:30:53 +0800

    Add exercise 2.43

---
+++ ex 2.43.rkt
@@ -0,0 +1,34 @@
+; Exercise 2.43
+
+; From exercise 2.42:
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+; Exercise 2.43:
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (new-row)
+            (map (lambda (rest-of-queens)
+                   (adjoin-position new-row k rest-of-queens))
+                 (queen-cols (- k 1))))
+          (enumerate-interval 1 board-size)))))
+  (queen-cols board-size))
+
+; The queens procedure in exercise 2.43 run very slowly because
+; the cost of running had increased as the program had called queen-cols procedure by (k * k) times.
